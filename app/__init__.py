25e4743bf44c81461acbd01a4076d3ae8536c350
trevor ngugi <ngugitrevor@gmail.com>
2020-01-15 17:25:33 +0300

    add pizza html

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 # Initializing application
 app = Flask(__name__)
+
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
 
 from app import views
